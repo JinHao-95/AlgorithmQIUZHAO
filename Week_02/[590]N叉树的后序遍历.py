6f6897aefa7847014855dd2ee046859e3ae42d54
JinHao-95 <hao.jin@nttdatabj.com.cn>
2020-07-26 14:31:28 +0800

    Week_02

---
+++ Week_02/[590]N叉树的后序遍历.py
@@ -0,0 +1,38 @@
+# 给定一个 N 叉树，返回其节点值的后序遍历。 
+# 
+#  例如，给定一个 3叉树 : 
+# 
+#  
+# 
+#  
+# 
+#  
+# 
+#  返回其后序遍历: [5,6,3,2,4,1]. 
+# 
+#  
+# 
+#  说明: 递归法很简单，你可以使用迭代法完成此题吗? Related Topics 树 
+#  👍 86 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        if root is None:
+            return []
+        res = []
+        for child in root.children:
+            res.extend(self.postorder(child))
+        res.append(root.val)
+        return res
+
+# leetcode submit region end(Prohibit modification and deletion)
